b7667032840227f42a5015da61741d47be17515f
Yunxiao Liu <yunxiao.liu@tum.de>
2025-03-23 14:57:04 +0100

    finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(some_string) {
+    // Each char goes the a new array with 
+    // new_index = str.length - own_index
+    let result = []
+    for (i=0;i<some_string.length;i++){
+        result[some_string.length-i] = some_string[i]
+    }
+    return result.join('')
 };
 
 // Do not edit below this line
