8635d4b75f619944f8c24a42896c74be1b390bb3
Yunxiao Liu <yunxiao.liu@tum.de>
2025-03-23 15:15:32 +0100

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(someArray,...theArgus) {
+    for (toRemove of theArgus){
+        someArray = someArray.filter(element => element !== toRemove)
+    }
+    return someArray
 };
 
 // Do not edit below this line
