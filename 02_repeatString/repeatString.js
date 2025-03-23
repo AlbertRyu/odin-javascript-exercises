4bd93ab1180f8e2239cf71e5c7086e87554d97a9
Yunxiao Liu <yunxiao.liu@tum.de>
2025-03-23 14:46:33 +0100

    finished exercise 1 + 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(someString, repeatTimes) {
+    let resultString = '';
+    if (repeatTimes < 0) {
+        return "ERROR"
+    }
+    while(repeatTimes) {
+        resultString += someString
+        repeatTimes --
+    }
+    return resultString
+}
 // Do not edit below this line
 module.exports = repeatString;
