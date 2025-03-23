30cccf8d2eab4ae90eace228f33599962803ba84
Yunxiao Liu <yunxiao.liu@tum.de>
2025-03-23 15:42:09 +0100

    finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(thisYear) {
+    // Two Scenorio:
+    // 1. Divisible by 100
+    // Then is LeapYear if divisiable by 400
+    // 2. Not Divisible by 100
+    // Then is LeapYear if divisible by 4
+    return thisYear % 100 === 0 ? (thisYear % 400 === 0) : (thisYear % 4 === 0)
 };
 
 // Do not edit below this line
