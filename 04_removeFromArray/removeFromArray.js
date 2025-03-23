cf4991f2c757cbefc15bb6efcf14c86a3f424bb6
Yunxiao Liu <yunxiao.liu@tum.de>
2025-03-23 15:31:19 +0100

    finishing exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,15 @@
 const removeFromArray = function(someArray,...theArgus) {
+
+    /*
     for (toRemove of theArgus){
         someArray = someArray.filter(element => element !== toRemove)
     }
     return someArray
+    */
+
+    //Better way.
+    return someArray.filter(ele => !theArgus.includes(ele))
+
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(one_end, another_end) {
 
+    if (one_end < 0 || another_end < 0){
+        return 'ERROR'
+    }
+
+    if (Math.floor(one_end) != one_end 
+    || Math.floor(another_end) != another_end) {
+        return 'ERROR'
+    }
+
+    if (typeof(one_end) !== 'number' 
+    || typeof(another_end) !== 'number') {
+        return 'ERROR'
+    }
+
+    // Determin which end is the left.
+    left_end = Math.min(one_end,another_end)
+    right_end = Math.max(one_end,another_end)
+
+    let sum = 0
+    for (i = left_end; i<=right_end; i++)
+        sum += i
+    return sum
 };
 
 // Do not edit below this line
