42208febc901acbe6103070522e69c3e87215a60
Yunxiao <yunxiao.liu@proton.me>
2025-04-01 19:12:00 +0200

    solved exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b
+};
+
+const subtract = function(a, b) {
+  return a - b
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+ return array.reduce((total, item) => total + item, 0)
+};
+
+const multiply = function(array) {
+	return array.reduce((total, item) => total * item, 1) 
+};
+
+const power = function(a, b) {
+	return a**b
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(size) {
+	if (size === 0 || size ===1){
+		return 1
+	} else {
+		return size * factorial(size-1)
+	}	
 };
 
 // Do not edit below this line
