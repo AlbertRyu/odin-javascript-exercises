56b9d6c88978e7e3aded7dc1e6b57a4de02a25eb
Yunxiao <yunxiao.liu@proton.me>
2025-04-01 19:32:45 +0200

    solved exerces 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    cleaned = string.replace(/[^\w]|_/g,'').toLowerCase()
+    reversed = [...cleaned].reverse().join('')
+    return cleaned === reversed
 };
 
 // Do not edit below this line
