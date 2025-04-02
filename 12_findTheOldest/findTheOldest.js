841b509cfe327077cc0af4308b43c0368cb3cf29
Yunxiao <yunxiao.liu@proton.me>
2025-04-02 10:05:27 +0200

    solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    currentYear = new Date().getFullYear()
+    oldest = people.reduce(
+        (currentOldest, next) => {
+            currentAge = (currentOldest.yearOfDeath||currentYear) - currentOldest.yearOfBirth
+            nextAge = (next.yearOfDeath||currentYear) - next.yearOfBirth
+            return (nextAge > currentAge) ? next : currentOldest
+        }
+    )
+    return oldest
 };
 
 // Do not edit below this line
