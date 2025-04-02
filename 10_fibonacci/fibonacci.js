cfd586e43e82cf9af371a9b23f1d8a3f7c67aa47
Yunxiao <yunxiao.liu@proton.me>
2025-04-02 09:31:32 +0200

    sovled exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index) // Incase user input strings.
+    if (index < 0) {
+        return "OOPS"
+    } else if (index === 0){
+        return 0
+    } else if (index === 1 | index === 2){
+        return 1
+    } else {
+        return fibonacci(index-1) + fibonacci(index-2)
+    }
 };
 
 // Do not edit below this line
